0c02cb714d75184106f2735d44856d87eeeade11
shavenking <shavenking@gmail.com>
2017-07-01 11:37:42 +0800

    Fix ExpenseTest bug

--- tests/Feature/ExpenseTest.php
+++ tests/Feature/ExpenseTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\PlatformFactory;
 use App\Transaction;
 use App\TransactionTypeFactory;
 use Carbon\Carbon;
@@ -13,7 +14,7 @@ class ExpenseTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testUserCanCreateExpenseViaTelegram()
+    public function testUserCanCreateExpenseViaTelegramWhenFirstTimeReachOurService()
     {
         $telegramFactory = app(TelegramFactory::class);
 
@@ -25,13 +26,53 @@ class ExpenseTest extends TestCase
             ->post('/api/webhooks/telegram/' . env('TELEGRAM_KEY'), $telegramUpdate)
             ->assertStatus(200);
 
+        $user = app(PlatformFactory::class)
+            ->getTelegram()
+            ->usersByPlatformUserId(array_get($telegramUpdate, 'message.from.id'))
+            ->firstOrFail();
+
         $expense = app(TransactionTypeFactory::class)->getExpense();
         $messageDate = app(Carbon::class)->createFromTimestamp(array_get($telegramUpdate, 'message.date'));
 
         $this->assertDatabaseHas(
             (new Transaction)->getTable(),
             [
-                'user_id' => array_get($telegramUpdate, 'message.from.id'),
+                'user_id' => $user->id,
+                'transaction_type_id' => $expense->id,
+                'amount' => '9487.00',
+                'balance' => '9487.00',
+                'created_at' => $messageDate,
+                'updated_at' => $messageDate,
+            ]
+        );
+    }
+
+    public function testUserCanCreateExpenseViaTelegram()
+    {
+        $telegramFactory = app(TelegramFactory::class);
+
+        $telegramUpdate = $telegramFactory->makeUpdate([
+            'message' => $telegramFactory->makeMessage(['text' => '9487 支出'])
+        ]);
+
+        $platformUserId = array_get($telegramUpdate, 'message.from.id');
+        $user = app(PlatformFactory::class)->getTelegram()->users()->create([
+            'name' => "TG-$platformUserId",
+            'email' => "EMAIL-$platformUserId",
+            'password' => ''
+        ], ['platform_user_id' => $platformUserId]);
+
+        $this
+            ->post('/api/webhooks/telegram/' . env('TELEGRAM_KEY'), $telegramUpdate)
+            ->assertStatus(200);
+
+        $expense = app(TransactionTypeFactory::class)->getExpense();
+        $messageDate = app(Carbon::class)->createFromTimestamp(array_get($telegramUpdate, 'message.date'));
+
+        $this->assertDatabaseHas(
+            (new Transaction)->getTable(),
+            [
+                'user_id' => $user->id,
                 'transaction_type_id' => $expense->id,
                 'amount' => '9487.00',
                 'balance' => '9487.00',
