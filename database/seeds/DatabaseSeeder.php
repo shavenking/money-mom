6b39bceb6094d783f37dd3c7f8b91e8c9294f517
shavenking <shavenking@gmail.com>
2017-07-07 21:21:22 +0800

    Fix coding style

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(TransactionTypeSeeder::class);
+        $this->call(TransactionTypeSeeder::class);
         $this->call(PlatformSeeder::class);
     }
 }
